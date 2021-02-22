c5fc3e285c34cb312424e0cb5d3c81869137c712
yongfangzhang <zhangyongfang_2021@126.com>
2021-02-22 18:59:55 +0800

    add generator

--- src/main/java/com/yihaokezhan/hotel/HotelApplication.java
+++ src/main/java/com/yihaokezhan/hotel/HotelApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.yihaokezhan.hotel.module.*.mapper")
+@MapperScan("com.yihaokezhan.hotel.mapper")
 public class HotelApplication {
 
     public static void main(String[] args) {
