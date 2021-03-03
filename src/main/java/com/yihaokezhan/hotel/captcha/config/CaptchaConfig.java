0cdbdf60ed9408288d12e8072197a64112843392
yongfangzhang <zhangyongfang_2021@126.com>
2021-03-03 13:35:59 +0800

    add captcha

---
+++ src/main/java/com/yihaokezhan/hotel/captcha/config/CaptchaConfig.java
@@ -0,0 +1,27 @@
+package com.yihaokezhan.hotel.captcha.config;
+
+import java.util.Properties;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class CaptchaConfig {
+
+    @Bean
+    public DefaultKaptcha producer() {
+        Properties properties = new Properties();
+        properties.put("kaptcha.border", "no");
+        properties.put("kaptcha.textproducer.font.color", "black");
+        properties.put("kaptcha.textproducer.char.space", "5");
+        properties.put("kaptcha.image.width", "200");
+        properties.put("kaptcha.image.height", "60");
+        properties.put("kaptcha.textproducer.font.size", "50");
+        Config config = new Config(properties);
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+    }
+}
