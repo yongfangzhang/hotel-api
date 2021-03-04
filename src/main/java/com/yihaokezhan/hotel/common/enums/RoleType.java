752c6fd71671365ad807ee83a603c4fe453ac40a
yongfangzhang <zhangyongfang_2021@126.com>
2021-03-04 17:08:13 +0800

    remove su

--- src/main/java/com/yihaokezhan/hotel/common/enums/RoleType.java
+++ src/main/java/com/yihaokezhan/hotel/common/enums/RoleType.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum RoleType implements BaseEnum<RoleType> {
     // @formatter:off
     UNKNOWN              (0,     "UNKNOWN",        "未知"),
-    SU                   (1,     "SU",             "超级管理员"),
+    // SU                   (1,     "SU",             "超级管理员"),
     ADMIN                (2,     "ADMIN",          "管理员"),
     STAFF                (3,     "STAFF",          "员工"),
     END                  (1000,  "END",            "END");
